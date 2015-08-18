a5a85038ca3c3a1e1692678f584655a636e54798
Evan Klitzke <evan@eklitzke.org>
2015-08-17 19:52:17 -0700

    get tut02 working

--- src/main.cc
+++ src/main.cc
@@ -1,55 +1,90 @@
+#include <fstream>
 #include <iostream>
+#include <memory>
+#include <string>
 
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-GLuint program;
+GLuint program, vbo_triangle;
 GLint attribute_coord2d;
 
-int init_resources(void) {
-  GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
-  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-  const char *vs_source =
+std::string ReadFile(const std::string &filename) {
+  std::ifstream f(filename);
+  return std::string(std::istreambuf_iterator<char>(f),
+                     std::istreambuf_iterator<char>());
+}
+
+void PrintLog(GLuint object) {
+  GLint log_length = 0;
+  if (glIsShader(object)) {
+    glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
+  } else if (glIsProgram(object)) {
+    glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
+  } else {
+    std::cerr << "PrintLog: not a shader or a program\n";
+    return;
+  }
+
+  std::unique_ptr<char[]> log(new char[log_length]);
+  if (glIsShader(object)) {
+    glGetShaderInfoLog(object, log_length, NULL, log.get());
+  } else if (glIsProgram(object)) {
+    glGetProgramInfoLog(object, log_length, NULL, log.get());
+  }
+  std::cerr << log.get();
+}
+
+GLuint CreateShader(const std::string &filename, GLenum type) {
+  const std::string source = ReadFile(filename);
+  if (!source.length()) {
+    std::cerr << "failed to read " << filename << "\n";
+    return 0;
+  }
+
+  GLuint res = glCreateShader(type);
+  const GLchar* sources[2] = {
 #ifdef GL_ES_VERSION_2_0
-    "#version 100\n"  // OpenGL ES 2.0
+    "#version 100\n"
+    "#define GLES2\n",
 #else
-    "#version 120\n"  // OpenGL 2.1
+    "#version 120\n",
 #endif
-    "attribute vec2 coord2d;"
-    "void main(void) {"
-    "  gl_Position = vec4(coord2d, 0.0, 1.0);"
-    "}";
-  glShaderSource(vs, 1, &vs_source, nullptr);
-  glCompileShader(vs);
-  glGetShaderiv(vs, GL_COMPILE_STATUS, &compile_ok);
+    source.c_str()
+  };
+  glShaderSource(res, 2, sources, NULL);
+
+  glCompileShader(res);
+
+  GLint compile_ok = GL_FALSE;
+  glGetShaderiv(res, GL_COMPILE_STATUS, &compile_ok);
   if (compile_ok == GL_FALSE) {
-    std::cerr << "error in vertex shader\n";
+    std::cerr << "%s: " << filename;
+    PrintLog(res);
+    glDeleteShader(res);
+    return 0;
+  }
+  return res;
+}
+
+int init_resources(void) {
+  GLuint vs, fs;
+  if ((vs = CreateShader("src/triangle.v.glsl", GL_VERTEX_SHADER)) == 0) {
+    return 0;
+  }
+  if ((fs = CreateShader("src/triangle.f.glsl", GL_FRAGMENT_SHADER)) == 0) {
     return 0;
   }
 
-  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-  const char *fs_source =
-    "#version 120\n"
-    "void main(void) {"
-    "  gl_FragColor[0] = gl_FragCoord.x/640.0;"
-    "  gl_FragColor[1] = gl_FragCoord.y/480.0;"
-    "  gl_FragColor[2] = 0.5;"
-    "}";
-  glShaderSource(fs, 1, &fs_source, nullptr);
-  glCompileShader(fs);
-  glGetShaderiv(fs, GL_COMPILE_STATUS, &compile_ok);
-  if (compile_ok == GL_FALSE) {
-    std::cerr << "error in fragment shader\n";
-    return 0;
-  }
-
+  GLint link_ok = GL_FALSE;
   program = glCreateProgram();
   glAttachShader(program, vs);
   glAttachShader(program, fs);
   glLinkProgram(program);
   glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
   if (link_ok == GL_FALSE) {
-    std::cerr << "error in glLinkProgram\n";
+    std::cerr << "glLinkProgram: ";
+    PrintLog(program);
     return 0;
   }
 
@@ -59,6 +94,16 @@ int init_resources(void) {
     std::cerr << "could not bind attribute: " << attribute_name << "\n";
     return 0;
   }
+
+  GLfloat triangle_vertices[] = {
+    0.0,  0.8,
+    -0.8, -0.8,
+    0.8, -0.8,
+  };
+  glGenBuffers(1, &vbo_triangle);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+
   return 1;
 }
 
@@ -67,20 +112,19 @@ void onDisplay() {
   glClear(GL_COLOR_BUFFER_BIT);
 
   glUseProgram(program);
-  glEnableVertexAttribArray(attribute_coord2d);
-  GLfloat triangle_vertices[] = {
-    0.0,  0.8,
-    -0.8, -0.8,
-    0.8, -0.8,
-  };
 
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
+  glEnableVertexAttribArray(attribute_coord2d);
   glVertexAttribPointer(
     attribute_coord2d,
     2,
     GL_FLOAT,
     GL_FALSE,
     0,
-    triangle_vertices);
+    0);
 
   glDrawArrays(GL_TRIANGLES, 0, 3);
   glDisableVertexAttribArray(attribute_coord2d);
@@ -90,18 +134,24 @@ void onDisplay() {
 
 void free_resources() {
   glDeleteProgram(program);
+  glDeleteBuffers(1, &vbo_triangle);
 }
 
 int main(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitContextVersion(2, 0);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+  glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
   glutInitWindowSize(640, 480);
   glutCreateWindow("hello world");
 
   GLenum glew_status = glewInit();
   if (glew_status != GLEW_OK) {
     std::cerr << "Error: " << glewGetErrorString(glew_status) << "\n";
+    return 1;
+  }
+
+  if (!GLEW_VERSION_2_0) {
+    std::cerr << "Error: your graphic card does not support OpenGL 2.0\n";
     return 1;
   }
 
